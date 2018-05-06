a20978e4f18ecbbe7377b7baa259f7fddcd107e3
luisalcantaralo <30807768+luisalcantaralo@users.noreply.github.com>
2018-05-05 20:26:26 -0500

    Update db.sql

--- docs/database/db.sql
+++ docs/database/db.sql
@@ -1,17 +1,25 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.5.2
+-- version 4.7.7
 -- https://www.phpmyadmin.net/
 --
--- Servidor: localhost:8889
--- Tiempo de generación: 06-05-2018 a las 03:00:25
--- Versión del servidor: 5.6.35
--- Versión de PHP: 7.1.1
+-- Servidor: localhost
+-- Tiempo de generación: 06-05-2018 a las 03:25:11
+-- Versión del servidor: 10.1.30-MariaDB
+-- Versión de PHP: 7.2.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
 --
--- Base de datos: `poo`
+-- Base de datos: `pointOfSale`
 --
 
 -- --------------------------------------------------------
@@ -25,6 +33,13 @@ CREATE TABLE `Categoria` (
   `nombre` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
   `descripcion` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `Categoria`
+--
+
+INSERT INTO `Categoria` (`idCategoria`, `nombre`, `descripcion`) VALUES
+(1, 'tecnologia', 'telefonos, accesorios, etc');
 
 -- --------------------------------------------------------
 
@@ -85,10 +100,17 @@ CREATE TABLE `Detalle_Venta` (
 CREATE TABLE `Producto` (
   `idProducto` int(11) NOT NULL,
   `nombre` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `descripcion` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `cantidad` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
   `precio` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
   `idCategoria` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `Producto`
+--
+
+INSERT INTO `Producto` (`idProducto`, `nombre`, `cantidad`, `precio`, `idCategoria`) VALUES
+(1, 'iPhone', '100', '100', 1);
 
 -- --------------------------------------------------------
 
@@ -139,8 +161,6 @@ CREATE TABLE `Venta` (
   `idVenta` int(11) NOT NULL,
   `fecha` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
   `total` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `pago` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `cambio` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
   `idUsuario` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
@@ -211,42 +231,50 @@ ALTER TABLE `Venta`
 -- AUTO_INCREMENT de la tabla `Categoria`
 --
 ALTER TABLE `Categoria`
-  MODIFY `idCategoria` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idCategoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
 --
 -- AUTO_INCREMENT de la tabla `Compra`
 --
 ALTER TABLE `Compra`
   MODIFY `idCompra` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
 --
 -- AUTO_INCREMENT de la tabla `Detalle_Compra`
 --
 ALTER TABLE `Detalle_Compra`
   MODIFY `idDetalle_Compra` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT de la tabla `Detalle_Venta`
 --
 ALTER TABLE `Detalle_Venta`
   MODIFY `idDetalle_Venta` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT de la tabla `Producto`
 --
 ALTER TABLE `Producto`
-  MODIFY `idProducto` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idProducto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
 --
 -- AUTO_INCREMENT de la tabla `Proveedor`
 --
 ALTER TABLE `Proveedor`
   MODIFY `idProveedor` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
 --
 -- AUTO_INCREMENT de la tabla `Usuario`
 --
 ALTER TABLE `Usuario`
   MODIFY `idUsuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
 --
 -- AUTO_INCREMENT de la tabla `Venta`
 --
 ALTER TABLE `Venta`
   MODIFY `idVenta` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- Restricciones para tablas volcadas
 --
@@ -282,3 +310,8 @@ ALTER TABLE `Producto`
 --
 ALTER TABLE `Venta`
   ADD CONSTRAINT `fk_Venta_Usuario1` FOREIGN KEY (`idUsuario`) REFERENCES `Usuario` (`idUsuario`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
