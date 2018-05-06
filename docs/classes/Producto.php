9dac8d3a2313eeb1977c7949e62537b82b20bb71
luisalcantaralo <30807768+luisalcantaralo@users.noreply.github.com>
2018-05-05 21:18:05 -0500

    Add files via upload

---
+++ docs/classes/Producto.php
@@ -0,0 +1,90 @@
+<?php require_once 'config/Conexion.php';
+Class Producto{
+  function __construct(){
+  }
+  function selectAll(){
+    $sql = "SELECT Producto.idProducto, Producto.nombreP, Producto.cantidad, Producto.precio, Producto.idCategoria, Categoria.nombreC, Categoria.descripcion FROM Producto INNER JOIN Categoria ON Producto.idCategoria=Categoria.idCategoria";
+    $response = array();
+    if($result = simpleQuery($sql)){
+      $response["status"] = 200;
+      $response["message"] = "";
+      $response["datos"] = array();
+      while($row = $result->fetch_assoc()){
+          array_push($response["datos"], $row);
+      }
+      echo json_encode($response);
+    }
+  }
+  function select($id){
+    $sql = "SELECT * FROM Producto WHERE idProducto = $id";
+    $response = array();
+    if($result = simpleQuery($sql)){
+      if($result->num_rows <= 0){
+        http_response_code(400);
+        $response["status"] = 400;
+        $response["message"] = "Error: no entries found!";
+        echo json_encode($response);
+        exit();
+      }
+      $response["status"] = 200;
+      $response["message"] = "Successful";
+      $response["datos"] = $result->fetch_assoc();
+      echo json_encode($response);
+    }
+  else {
+    http_response_code(400);
+    $response["status"] = 400;
+    $response["message"] = "Error: " . showError();
+    echo json_encode($response);
+  }
+}
+  function insert($nombre, $descripcion, $precio, $idCategoria){
+    $sql = "INSERT INTO Producto(nombre, descripcion, precio, idCategoria) VALUES('$nombre', '$descripcion','$precio', $idCategoria)";
+    if(simpleQuery($sql)){
+      $response["status"] = 201;
+      $response["message"] = "Successful, entries added!";
+      echo json_encode($response);
+    }
+    else {
+      http_response_code(400);
+      $response["status"] = 400;
+      $response["message"] = "Error: " . showError();
+      echo json_encode($response);
+    }
+  }
+  function update($id, $nombre, $descripcion, $precio, $idCategoria){
+    $sql = "UPDATE Producto SET nombre='$nombre', descripcion='$descripcion', precio='$precio',idCategoria=$idCategoria WHERE idProducto=$id";
+    if(simpleQuery($sql)){
+      $response["status"] = 201;
+      $response["message"] = "Successful, entries updated!";
+      echo json_encode($response);
+    }
+    else {
+      http_response_code(400);
+      $response["status"] = 400;
+      $response["message"] = "Error: " . showError();
+      echo json_encode($response);
+    }
+  }
+  function delete($id){
+    $sql = "DELETE FROM Producto WHERE idProducto=$id";
+    if(simpleQuery($sql)){
+      $response["status"] = 201;
+      $response["message"] = "Successful, entry removed!";
+      echo json_encode($response);
+    }
+    else {
+      http_response_code(400);
+      $response["status"] = 400;
+      $response["message"] = "Error: " . showError();
+      echo json_encode($response);
+    }
+  }
+  function error($message){
+    http_response_code(400);
+    $response["status"] = 400;
+    $response["message"] = $message;
+    echo json_encode($response);
+  }
+}
+?>
---
+++ docs/classes/Venta.php
@@ -0,0 +1,94 @@
+<?php require_once 'config/Conexion.php';
+
+Class Venta{
+
+  function __construct(){
+  }
+  function selectAll(){
+    $sql = "SELECT * FROM Venta";
+    $response = array();
+    if($result = simpleQuery($sql)){
+      $response["status"] = 200;
+      $response["message"] = "";
+      $datos = array();
+      while($row = $result->fetch_assoc()){
+          array_push($datos, $row);
+      }
+      echo json_encode($datos);
+    }
+  }
+  function select($id){
+    $sql = "SELECT * FROM Venta WHERE idVenta = $id";
+    $response = array();
+
+    if($result = simpleQuery($sql)){
+      if($result->num_rows <= 0){
+        http_response_code(400);
+        $response["status"] = 400;
+        $response["message"] = "Error: no entries found!";
+        echo json_encode($response);
+        exit();
+      }
+      $response["status"] = 200;
+      $response["message"] = "Successful";
+      $response["datos"] = $result->fetch_assoc();
+      echo json_encode($response);
+
+    }
+  else {
+    http_response_code(400);
+    $response["status"] = 400;
+    $response["message"] = "Error: " . showError();
+    echo json_encode($response);
+  }
+}
+  function insert($fecha, $total, $idUsuario){
+    $sql = "INSERT INTO Venta(fecha, total, idUsuario) VALUES('$fecha', '$total',idUsuario)";
+    if(simpleQuery($sql)){
+      $response["status"] = 201;
+      $response["message"] = "Successful, entries added!";
+      echo json_encode($response);
+    }
+    else {
+      http_response_code(400);
+      $response["status"] = 400;
+      $response["message"] = "Error: " . showError();
+      echo json_encode($response);
+    }
+  }
+  function update($id, $fecha, $total, $idUsuario){
+    $sql = "UPDATE Venta SET fecha='$fecha', total='$total', idUsuario=$idUsuario WHERE idVenta=$id";
+    if(simpleQuery($sql)){
+      $response["status"] = 201;
+      $response["message"] = "Successful, entries updated!";
+      echo json_encode($response);
+    }
+    else {
+      http_response_code(400);
+      $response["status"] = 400;
+      $response["message"] = "Error: " . showError();
+      echo json_encode($response);
+    }
+  }
+  function delete($id){
+    $sql = "DELETE FROM Venta WHERE idVenta=$id";
+    if(simpleQuery($sql)){
+      $response["status"] = 201;
+      $response["message"] = "Successful, entry removed!";
+      echo json_encode($response);
+    }
+    else {
+      http_response_code(400);
+      $response["status"] = 400;
+      $response["message"] = "Error: " . showError();
+      echo json_encode($response);
+    }
+  }
+  function error($message){
+    http_response_code(400);
+    $response["status"] = 400;
+    $response["message"] = $message;
+    echo json_encode($response);
+  }
+}
+?>
